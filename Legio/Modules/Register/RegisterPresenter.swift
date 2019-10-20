cdcf34ba23d14507fb08687576d442836f846302
Marat <iknow.whoiam@mail.ru>
2019-10-20 18:06:23 +0300

    Register - реализовал динамическое изменение view при вводе данных

--- Legio/Modules/Register/RegisterPresenter.swift
+++ Legio/Modules/Register/RegisterPresenter.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 protocol RegisterPresenterProtocol: class {
-    func registrateTapped(email: String?, password: String?)
+    func registrateTapped()
+    func changed(login: String?)
+    func changed(password: String?)
 }
 
 class RegisterPresenter {
@@ -17,22 +19,48 @@ class RegisterPresenter {
     weak var view: RegisterViewProtocol?
     var interactor: RegisterInteractorProtocol!
     var router: RegisterRouterProtocol!
-    let chekLoginPasswprd = EnableButton()
-
     
+    private var login: String?
+    private var password: String?
+
+    private let validImage = UIImage(named: "checkMarkTF")
     private let incorrectDataError = "You set incorrect data"
     
 }
 
 extension RegisterPresenter: RegisterPresenterProtocol {
     
-    func registrateTapped(email: String?, password: String?) {
-        guard let email = chekLoginPasswprd.validate(email: email),
-            let password = chekLoginPasswprd.validate(password: password) else {
+    func registrateTapped() {
+        guard let email = login,
+            let password = password else {
                 self.view?.show(error: incorrectDataError)
                 return
         }
         registrate(email: email, password: password)
+    }
+    
+    func changed(login: String?) {
+        var isValidated = false
+        if let validateLogin = interactor.checkValid(login: login) {
+            self.login = validateLogin
+            isValidated = true
+        } else {
+            self.login = nil
+        }
+        updateLoginViews(isValidated: isValidated)
+        updateButtonRegister()
+    }
+    
+    func changed(password: String?) {
+        var isValidated = false
+        if let validatePassword = interactor.checkValid(password: password) {
+            self.password = validatePassword
+            isValidated = true
+        } else {
+            self.password = nil
+        }
+        updatePasswordViews(isValidated: isValidated)
+        updateButtonRegister()
     }
 }
 
@@ -50,6 +78,51 @@ extension RegisterPresenter {
                 self?.view?.show(error: errorText)
             }
         }
+    }
+    
+    private func updateLoginViews(isValidated: Bool) {
+        view?.updateLoginAlert(isHidden: isValidated)
+        
+        let validateProgress = getProgress(isValidated: isValidated)
+        view?.updateLoginProgressBar(progress: validateProgress)
+        
+        let shownImage = getValidImage(isValidated: isValidated)
+        view?.updateLoginSuccess(image: shownImage)
+    }
+    
+    private func updatePasswordViews(isValidated: Bool) {
+        view?.updatePasswordAlert(isHidden: isValidated)
+        
+        let validateProgress = getProgress(isValidated: isValidated)
+        view?.updatePasswordProgressBar(progress: validateProgress)
+        
+        let shownImage = getValidImage(isValidated: isValidated)
+        view?.updatePasswordSuccess(image: shownImage)
+    }
+    
+    private func updateButtonRegister() {
+           let buttonRegisterIsEnabled = isCorrectData()
+        view?.updateButtonRegister(isEnabled: buttonRegisterIsEnabled)
+       }
+    
+    private func getProgress(isValidated: Bool) -> Float {
+        return isValidated
+            ? 0.0
+            : 1.0
+    }
+    
+    private func getValidImage(isValidated: Bool) -> UIImage? {
+        return isValidated
+            ? validImage
+            : nil
+    }
+    
+    private func isCorrectData() -> Bool {
+        guard self.login != nil ,
+            self.password != nil else {
+                return false
+        }
+        return true
     }
     
 }
