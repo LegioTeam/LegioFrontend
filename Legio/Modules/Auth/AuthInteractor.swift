e79fbad7ad4271d396951ccf58a4e0f74bbabc95
Marat <iknow.whoiam@mail.ru>
2019-10-20 18:05:44 +0300

    ≠

--- Legio/Modules/Auth/AuthInteractor.swift
+++ Legio/Modules/Auth/AuthInteractor.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 protocol AuthInteractorProtocol {
+    func checkValid(login: String?) -> String?
+    func checkValid(password: String?) -> String?
     func auth(email: String, password: String, completion: @escaping(_ userData: Success?, _ error: Error?) -> Void)
 }
 
 class AuthInteractor: AuthInteractorProtocol {
-    
+    private let validateManager = ValidateManager()
     var network = NetworkManager.shared
+    
     func auth(email: String, password: String, completion: @escaping (_ userData: Success?,_ error: Error?) -> Void) {
         network.userAuth(login: email, password: password, completion: completion)
     }
+    
+    internal func checkValid(login: String?) -> String? {
+        return validateManager.validate(email: login)
+    }
+    
+    internal func checkValid(password: String?) -> String? {
+        return validateManager.validate(password: password)
+    }
+    
 }
